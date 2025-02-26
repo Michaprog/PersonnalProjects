d2a8888bbcb10dd656cf5f564ed413b7ab99ea2e
Michaprog <vejnstejn2@gmail.com>
2025-02-26 12:38:53 +0100

    change in git

---
+++ MyGas/src/main/java/com/yourcompany/garage/mygas/MyGasApplication.java
@@ -0,0 +1,13 @@
+package com.yourcompany.garage.mygas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGasApplication.class, args);
+    }
+
+}
---
+++ MyGas/src/test/java/com/yourcompany/garage/mygas/MyGasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcompany.garage.mygas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
